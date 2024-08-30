5651cba4b2530a1e296986cfb2a2946cb1c786ea
一页书 <96804858+pili-book@users.noreply.github.com>
2024-08-30 15:58:49 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -110,7 +110,7 @@ async def send_telegram_message(message):
         send_data = {
             "msgtype": "text",  # 消息类型
             "text": {
-                "content": "serve00的登陆信息"+message,  # 文本内容，最长不超过2048个字节，必须是utf8编码
+                "content": "serve00的登陆信息"+"message",  # 文本内容，最长不超过2048个字节，必须是utf8编码
                 "mentioned_list": ["@all"],
                 # userid的列表，提醒群中的指定成员(@某个成员)，@all表示提醒所有人，如果开发者获取不到userid，可以使用mentioned_mobile_list
                 "mentioned_mobile_list": ["13934579714"]  # 手机号列表，提醒手机号对应的群成员(@某个成员)，@all表示提醒所有人
